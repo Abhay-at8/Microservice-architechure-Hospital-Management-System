e192fd3523e1066377d3ad9ed900db699c5fc5c0
Abhay <abhaytripathi98.com>
2024-07-04 17:34:31 +0530

    modified gateway

--- gateway/api-gateway/src/main/java/com/sa/apigateway/ApiGatewayApplication.java
+++ gateway/api-gateway/src/main/java/com/sa/apigateway/ApiGatewayApplication.java
@@ -1,16 +1,13 @@
-package com.sa.gateway;
+package com.sa.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
-
 
 @SpringBootApplication
-public class GatewayApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
